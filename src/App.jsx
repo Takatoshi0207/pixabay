32fe0c847c6856e93acf994912782ac6bdb8e6a5
Takatoshi0207 <t.kotani@m-okuchu.com>
2022-10-03 16:36:15 +0900

    検索バーの作成完了

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './App.css'
 import Searchbar from './componets/Searchbar'
 
 function App() {
-
+  const onSearchSubmit = (term) => {
+    console.log(term);
+  };
   return (
-    <div className="App">
-      < Searchbar />
+    <div className="ui container" style={{ marginTop: '20px' }}>
+      < Searchbar onSubmit={onSearchSubmit} />
     </div>
   )
 }
--- src/componets/Searchbar.jsx
+++ src/componets/Searchbar.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Searchbar = () => {
+const Searchbar = ({ onSubmit }) => {
+  const [term, setTerm] = useState('');
+
   const onFormSubmit = (e) => {
     e.preventDefault();
-    console.log('確認OK!');
+    // console.log('確認OK!');
+    onSubmit(term);
+    // setTerm("");
   };
 
   return (
     <form onSubmit={onFormSubmit} className="ui form">
       <div className="field">
         <label>First Name</label>
-        <input type="text" name="first-name" placeholder=" " />
+        <input
+          type="text"
+          name="search"
+          placeholder=" "
+          value={term}
+          onChange={(e) => {
+            setTerm(e.target.value);
+          }} />
       </div>
     </form >
   )
