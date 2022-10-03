7c2a31c787b85f96d25f15b5ff328411b62d1391
Takatoshi0207 <t.kotani@m-okuchu.com>
2022-10-03 19:35:57 +0900

    画像の一覧表示完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css'
+import axios from 'axios';
 import Searchbar from './componets/Searchbar'
+import { useState } from 'react';
+import ImageList from './componets/ImageList';
 
 function App() {
-  const onSearchSubmit = (term) => {
-    console.log(term);
+  const [images, setImages] = useState([]);
+  const onSearchSubmit = async (term) => {
+    try {
+      const params = {
+        key: '30334115-ab7a210868caff303b6eb85e3',
+        q: term,
+      };
+      const responce = await axios.get('https://pixabay.com/api/', { params });
+      // console.log(responce)
+      setImages(responce.data.hits);
+    } catch (error) {
+    }
   };
   return (
     <div className="ui container" style={{ marginTop: '20px' }}>
       < Searchbar onSubmit={onSearchSubmit} />
+      < ImageList images={images} />
     </div>
   )
 }
